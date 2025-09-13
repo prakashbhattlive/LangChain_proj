5ea8205bb815e467fff12a7a50d8854693275164
Prakash Bhatt <prakashbhatt@live.com>
2025-09-13 13:02:27 +0000

    setting initials

---
+++ main.py
@@ -0,0 +1,10 @@
+from dotenv import load_dotenv
+import os
+load_dotenv()  # Load environment variables from .env file
+
+def main():
+    print("Hello from langchain-course!")
+
+
+if __name__ == "__main__":
+    main()
